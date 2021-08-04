8c1d5affe6190f7bd028fe8057a993b8029939ba
Jeff Davidson <jpd236@cornell.edu>
2021-08-03 22:20:26 -0700

    1.2.0 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 group = "com.jeffpdavidson"
-version = "1.1.1-SNAPSHOT"
+version = "1.2.0"
 
 repositories {
     mavenCentral()
-    // TODO: Remove ahead of public release.
-    maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+    // TODO: Reenable for next SNAPSHOT.
+    // maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
 }
 
 dependencies {
@@ -18,7 +18,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
     implementation("com.github.ajalt.colormath:colormath:2.1.0")
 
-    implementation("com.jeffpdavidson.kotwords:kotwords-js:1.1.1-SNAPSHOT")
+    implementation("com.jeffpdavidson.kotwords:kotwords-js:1.2.0")
 
     runtimeOnly(npm("webextension-polyfill", "0.7.0"))
     runtimeOnly(npm("jquery", "3.5.1"))

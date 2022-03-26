12518a192bff2de149803e77fff69353430db13d
Jeff Davidson <jpd236@cornell.edu>
2022-03-26 15:57:17 -0700

    More flexible NYT scraping.
    - If we can't find the filename/source in the page's gamedata, try to
      infer it from the current URL. No known issues here, but might provide
      more flexibility in the case of a future change.
    
    - Try downloading from the NYT site directly, which appears to work, and
      which will automatically have the right cookies. If this fails, fall
      back to the API request as before. At least one user is seeing 403
      errors with the API but not the NYT site, so this should fix that.

--- src/main/kotlin/com/jeffpdavidson/crosswordscraper/sources/NewYorkTimesSource.kt
+++ src/main/kotlin/com/jeffpdavidson/crosswordscraper/sources/NewYorkTimesSource.kt
@@ -11,6 +11,8 @@ import org.w3c.dom.url.URL
 
 object NewYorkTimesSource : FixedHostSource() {
 
+    private val KNOWN_URL_PATTERN = "(daily|mini|bonus)/(\\d{4})/(\\d{2})/(\\d{2})".toRegex()
+
     override val sourceName = "New York Times"
     override val neededHostPermissions = listOf("https://*.nytimes.com/*")
 
@@ -36,36 +38,60 @@ object NewYorkTimesSource : FixedHostSource() {
             return getNewYorkTimesScrapeResult(NewYorkTimes.fromPluribus(pluribus, Http::fetchAsBinary))
         }
 
-        // Next, inspect the embedded gameData variable, which may contain the filename to use for another API call to
-        // obtain the puzzle data.
-        val gameDataString = Scraping.readGlobalJson(frameId, "gameData")
-        if (gameDataString.isNotEmpty()) {
-            val gameData = JSON.parse<GameDataJson>(gameDataString)
-            if (gameData.filename?.isNotEmpty() == true) {
-                val puzzleJsonUrl =
-                    "https://nyt-games-prd.appspot.com/svc/crosswords/v6/puzzle/${gameData.filename}.json"
+        // Otherwise, figure out the type and date so we can make a call to the NYT API.
+        val (filename, stream) = getJsonFilenameAndStream(frameId, url) ?: ("" to "")
+        if (filename.isNotEmpty()) {
+            // For simplicity, request permissions to both the main site and the API site, even though we might only
+            // need them for the main site.
+            val apiUrl = "https://nyt-games-prd.appspot.com/svc/crosswords/v6/puzzle/${filename}.json"
+            val neededPermissions = neededHostPermissions + getPermissionsForUrls(listOf(URL(apiUrl)))
+            if (!hasPermissions(neededPermissions)) {
+                return ScrapeResult.NeedPermissions(neededPermissions)
+            }
 
-                // Need permission to access NYT cookies and API.
-                val neededPermissions = neededHostPermissions + getPermissionsForUrls(listOf(URL(puzzleJsonUrl)))
-                if (!hasPermissions(neededPermissions)) {
-                    return ScrapeResult.NeedPermissions(neededPermissions)
-                }
-
-                // Obtain the NYT-S cookie for the current URL so we can use it in the API request.
+            val puzzleJson = try {
+                // First, try downloading the puzzle data directly from the NYT site.
+                Http.fetchAsString("https://www.nytimes.com/svc/crosswords/v6/puzzle/${filename}.json")
+            } catch (e: Http.HttpException) {
+                // Fall back to the API site, which needs the NYT-S cookie for the current URL as a request header.
                 val nytSCookie = browser.cookies.get(Details {
                     name = "NYT-S"
                     this@Details.url = url.toString()
                 }).await()
+
                 if (nytSCookie.value.isNotEmpty()) {
-                    val puzzleJson = Http.fetchAsString(puzzleJsonUrl, listOf("nyt-s" to nytSCookie.value))
-                    return getNewYorkTimesScrapeResult(
-                        NewYorkTimes.fromApiJson(puzzleJson, gameData.stream ?: "daily", Http::fetchAsBinary)
-                    )
+                    Http.fetchAsString(apiUrl, listOf("nyt-s" to nytSCookie.value))
+                } else {
+                    ""
                 }
+            }
+
+            if (puzzleJson.isNotEmpty()) {
+                return getNewYorkTimesScrapeResult(NewYorkTimes.fromApiJson(puzzleJson, stream, Http::fetchAsBinary))
             }
         }
 
         return ScrapeResult.Success(listOf())
+    }
+
+    private suspend fun getJsonFilenameAndStream(frameId: Int, url: URL): Pair<String, String>? {
+        // First, inspect the embedded gameData variable, which may contain the data.
+        val gameDataString = Scraping.readGlobalJson(frameId, "gameData")
+        if (gameDataString.isNotEmpty()) {
+            val gameData = JSON.parse<GameDataJson>(gameDataString)
+            if (gameData.filename?.isNotEmpty() == true) {
+                return gameData.filename!! to (gameData.stream ?: "daily")
+            }
+        }
+
+        // Next, see if the URL matches a known format; if so, we can convert that to the filename and stream.
+        val urlMatcher = KNOWN_URL_PATTERN.find(url.pathname)
+        if (urlMatcher != null) {
+            val parts = urlMatcher.groupValues
+            return "${parts[1]}/${parts[2]}-${parts[3]}-${parts[4]}" to parts[1]
+        }
+
+        return null
     }
 
     private suspend fun getNewYorkTimesScrapeResult(nyt: NewYorkTimes): ScrapeResult {

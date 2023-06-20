82871e853a5e13ffd3066125d1433b59cfb32ef6
Jeff Davidson <jpd236@cornell.edu>
2023-06-19 23:02:32 -0700

    Fix Puzzle Society scraping.
    The puzzle data no longer appears to be embedded in a global variable on
    the page. We take an approach similar to Crosshare - look in the
    embedded window.__NEXT_DATA__ variable for a URL that contains the raw
    puzzle data, and if it points to the wrong puzzle (due to navigating to
    another puzzle from the first one), then refetch the page at the current
    URL and check __NEXT_DATA__ there instead.

--- src/main/kotlin/com/jeffpdavidson/crosswordscraper/sources/PuzzleSocietySource.kt
+++ src/main/kotlin/com/jeffpdavidson/crosswordscraper/sources/PuzzleSocietySource.kt
@@ -1,47 +1,128 @@
 package com.jeffpdavidson.crosswordscraper.sources
 
+import com.jeffpdavidson.crosswordscraper.Http
 import com.jeffpdavidson.crosswordscraper.Scraping
+import com.jeffpdavidson.crosswordscraper.sources.Source.Companion.hostIsDomainOrSubdomainOf
 import com.jeffpdavidson.kotwords.formats.UclickJpz
 import com.jeffpdavidson.kotwords.formats.UclickXml
 import com.soywiz.klock.DateFormat
 import com.soywiz.klock.DateTime
 import com.soywiz.klock.parseDate
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.w3c.dom.parsing.DOMParser
 import org.w3c.dom.url.URL
 
 object PuzzleSocietySource : FixedHostSource() {
 
     override val sourceName: String = "Puzzle Society"
-    override fun neededHostPermissions(url: URL) = listOf("https://crossword-game.azureedge.net/*")
+    override fun neededHostPermissions(url: URL) =
+        listOf("https://*.puzzlesociety.com/*", "https://*.amuniversal.com/*")
 
     private val DATE_FORMAT = DateFormat("yyyy-MM-dd")
+    private val JSON = Json {
+        ignoreUnknownKeys = true
+    }
 
     override fun matchesUrl(url: URL): Boolean {
-        return url.host == "crossword-game.azureedge.net"
+        return url.hostIsDomainOrSubdomainOf("puzzlesociety.com")
     }
 
     override suspend fun scrapePuzzlesWithPermissionGranted(url: URL, tabId: Int, frameId: Int): ScrapeResult {
+        // First, try reading the JSON from the page's __NEXT_DATA__ directly.
         val scrapeFn = js(
             """function() {
-                var xmlData = game.scene.getScene('Crossword').crossword.xmlData.documentElement.outerHTML;
-                return xmlData ? xmlData : '';
+                var data = window.__NEXT_DATA__;
+                return data ? JSON.stringify(data) : '';
             }"""
         )
-        val puzzleXml = Scraping.executeFunctionForString(tabId, frameId, scrapeFn)
-        if (puzzleXml.isNotEmpty()) {
-            val dateFn = js("function() { return levelData.date ? levelData.date : ''; }")
-            val dateText = Scraping.executeFunctionForString(tabId, frameId, dateFn)
-            val date = if (dateText.isNotEmpty()) DATE_FORMAT.parseDate(dateText) else DateTime.now().date
-            return ScrapeResult.Success(
-                listOf(
-                    // Detect the XML format from the contents. The Modern Crossword uses JPZ; others use Uclick XML.
-                    if (puzzleXml.contains("<crossword-compiler")) {
-                        UclickJpz(puzzleXml, date)
-                    } else {
-                        UclickXml(puzzleXml, date)
-                    }
-                )
-            )
+        val dataJson = Scraping.executeFunctionForString(tabId, frameId, scrapeFn)
+        if (dataJson.isEmpty()) {
+            return ScrapeResult.Error("No data found on current page")
         }
-        return ScrapeResult.Success(listOf())
+        var data = getDataIfMatching(dataJson, url)
+        if (data == null) {
+            // Data doesn't match, so fetch the current page and read the embedded __NEXT_DATA__ initializer script.
+            val urlPermissions = neededHostPermissions(url) + getPermissionsForUrls(listOf(url))
+            if (!hasPermissions(urlPermissions)) {
+                return ScrapeResult.NeedPermissions(neededHostPermissions(url))
+            }
+            val html = Http.fetchAsString(url.toString())
+            val parser = DOMParser()
+            val document = parser.parseFromString(html, "text/html")
+            val nextDataJson = document.getElementById("__NEXT_DATA__")?.textContent ?: ""
+            if (nextDataJson.isEmpty()) {
+                return ScrapeResult.Error("No data found on refetched page")
+            }
+            data = getDataIfMatching(nextDataJson, url)
+        }
+        if (data == null) {
+            return ScrapeResult.Error("No matching data found")
+        }
+        val levelData = data.props.pageProps.gameContent.gameLevelDataSets[0]
+        val dateText = levelData.issueDate
+        val date = if (dateText.isNotEmpty()) DATE_FORMAT.parseDate(dateText) else DateTime.now().date
+        val xmlFiles = levelData.files.filter { it.mimeType in listOf("text/html", "application/xml") }
+        if (xmlFiles.isEmpty() || xmlFiles.last().url.isEmpty()) {
+            return ScrapeResult.Error("No file found in level data")
+        }
+        val xmlUrl = xmlFiles.last().url
+        val neededPermissions = getPermissionsForUrls(listOf(URL(xmlUrl)))
+        if (!hasPermissions(neededPermissions)) {
+            return ScrapeResult.NeedPermissions(neededPermissions)
+        }
+        val puzzleXml = Http.fetchAsString(xmlUrl)
+        if (puzzleXml.isEmpty()) {
+            return ScrapeResult.Error("Could not fetch puzzle XML")
+        }
+        return ScrapeResult.Success(
+            listOf(
+                // Detect the XML format from the contents. The Modern Crossword uses JPZ; others use Uclick XML.
+                if (puzzleXml.contains("<crossword-compiler")) {
+                    UclickJpz(puzzleXml, date)
+                } else {
+                    UclickXml(puzzleXml, date)
+                }
+            )
+        )
+    }
+
+    private fun getDataIfMatching(dataJson: String, url: URL): Data? {
+        val data = JSON.decodeFromString<Data>(dataJson)
+        val pathParts = url.pathname.split("/")
+        return if (pathParts.size > data.query.game.size &&
+            data.query.game == pathParts.subList(pathParts.size - data.query.game.size, pathParts.size)
+        ) data else null
+    }
+
+    @Serializable
+    private data class Data(
+        val props: Props,
+        val query: Query,
+    ) {
+        @Serializable
+        data class Props(val pageProps: PageProps) {
+            @Serializable
+            data class PageProps(val gameContent: GameContent) {
+                @Serializable
+                data class GameContent(val gameLevelDataSets: List<LevelData>) {
+                    @Serializable
+                    data class LevelData(
+                        val issueDate: String,
+                        val files: List<File>
+                    ) {
+                        @Serializable
+                        data class File(
+                            val url: String,
+                            val mimeType: String,
+                        )
+                    }
+                }
+            }
+        }
+
+        @Serializable
+        data class Query(val game: List<String>)
     }
 }

2bf734d2e9d4005da45541b20b03c38c8c03465c
Jeff Davidson <jpd236@cornell.edu>
2025-06-16 21:22:31 -0700

    Always use complete data when scraping Crosshare puzzles.
    Previously, if the current puzzle didn't match the embedded JSON data,
    we would call the API to obtain a PUZ file directly. However, PUZ files
    are lacking some of the richer features that Crosshare supports. Since
    we can now handle those features in the Crosshare parser, update the
    fallback to fetch the full data JSON instead of the exported PUZ file,
    which ensures consistent results regardless of how the page was loaded
    before scraping.
    
    See #52

--- src/jsMain/kotlin/com/jeffpdavidson/crosswordscraper/sources/CrosshareSource.kt
+++ src/jsMain/kotlin/com/jeffpdavidson/crosswordscraper/sources/CrosshareSource.kt
@@ -5,11 +5,14 @@ import com.jeffpdavidson.crosswordscraper.Scraping
 import com.jeffpdavidson.crosswordscraper.sources.Source.Companion.hostIsDomainOrSubdomainOf
 import com.jeffpdavidson.kotwords.formats.AcrossLite
 import com.jeffpdavidson.kotwords.formats.Crosshare
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import org.w3c.dom.url.URL
 
 object CrosshareSource : FixedHostSource() {
 
-    private val PUZZLE_ID_PATTERN = "(?:crosswords|embed)/([^/]+)".toRegex()
+    private val PUZZLE_ID_PATTERN = "(?:crosswords|embed)/(.+)".toRegex()
 
     override val sourceName = "Crosshare"
     override fun neededHostPermissions(url: URL) = listOf("https://*.crosshare.org/*")
@@ -28,35 +31,40 @@ object CrosshareSource : FixedHostSource() {
                 return window.__NEXT_DATA__ ? JSON.stringify(window.__NEXT_DATA__) : '';
             }"""
         )
-        val puzzleJson = Scraping.executeFunctionForString(tabId, frameId, scrapeFn)
-        if (puzzleJson.isNotEmpty()) {
-            try {
-                val crosshare = Crosshare(puzzleJson)
-                val puzzle = crosshare.asPuzzle()
-                // For the main Crosshare site, make sure the puzzle title matches the page title - otherwise, this data
-                // is probably for a different puzzle. For embeds, we don't need to do this since there is no navigation
-                // to different puzzles.
-                if (url.pathname.contains("/embed/")) {
-                    return ScrapeResult.Success(listOf(crosshare))
-                }
-                val titleFn = js("function() { return document.title; }")
-                val pageTitle = Scraping.executeFunctionForString(tabId, frameId, titleFn)
-                if (pageTitle.startsWith(puzzle.title)) {
-                    return ScrapeResult.Success(listOf(crosshare))
-                } else {
-                    console.info("JSON in __NEXT_DATA__ is for a different puzzle; falling back to PUZ API")
-                }
-            } catch (e: Exception) {
-                console.info("Could not load JSON from __NEXT_DATA__; falling back to PUZ API")
+        val nextDataJsonString = Scraping.executeFunctionForString(tabId, frameId, scrapeFn)
+        if (nextDataJsonString.isEmpty()) {
+            return ScrapeResult.Success(listOf())
+        }
+        try {
+            val crosshare = Crosshare(nextDataJsonString)
+            val puzzle = crosshare.asPuzzle()
+            // For the main Crosshare site, make sure the puzzle title matches the page title - otherwise, this data
+            // is probably for a different puzzle. For embeds, we don't need to do this since there is no navigation
+            // to different puzzles.
+            if (url.pathname.contains("/embed/")) {
+                return ScrapeResult.Success(listOf(crosshare))
             }
+            val titleFn = js("function() { return document.title; }")
+            val pageTitle = Scraping.executeFunctionForString(tabId, frameId, titleFn)
+            if (pageTitle.startsWith(puzzle.title)) {
+                return ScrapeResult.Success(listOf(crosshare))
+            } else {
+                console.info("JSON in __NEXT_DATA__ is for a different puzzle; falling back to API")
+            }
+        } catch (e: Exception) {
+            console.info("Could not load JSON from __NEXT_DATA__; falling back to API")
         }
 
-        // Fall back to the PUZ API - this requires a separate fetch, but should always work.
-        val matchResult = PUZZLE_ID_PATTERN.find(url.toString()) ?: return ScrapeResult.Success(listOf())
+        // Otherwise, fetch the JSON data directly - this requires a separate fetch, but should always work.
+        val nextDataJson = Json.parseToJsonElement(nextDataJsonString)
+        val buildId =
+            nextDataJson.jsonObject["buildId"]?.jsonPrimitive?.content ?: return ScrapeResult.Success(listOf())
+        val matchResult = PUZZLE_ID_PATTERN.find(url.pathname) ?: return ScrapeResult.Success(listOf())
         if (!hasPermissions(neededHostPermissions(url))) {
             return ScrapeResult.NeedPermissions(neededHostPermissions(url))
         }
         val puzzleId = matchResult.groupValues[1]
-        return ScrapeResult.Success(listOf(AcrossLite(Http.fetchAsBinary("https://crosshare.org/api/puz/$puzzleId"))))
+        val dataUrl = "https://crosshare.org/_next/data/$buildId/crosswords/$puzzleId.json"
+        return ScrapeResult.Success(listOf(Crosshare(Http.fetchAsString(dataUrl))))
     }
 }
